35f091f630de45aea3fa294f0387fff50a038bfd
sxmxc <steven.mcneely@live.com>
2016-01-21 21:28:50 -0600

    initial commit

---
+++ public/js/randomGame.js
@@ -0,0 +1,21 @@
+var board,
+  game = new Chess();
+
+var makeRandomMove = function() {
+  var possibleMoves = game.moves();
+
+  // exit if the game is over
+  if (game.game_over() === true ||
+    game.in_draw() === true ||
+    possibleMoves.length === 0) return;
+
+  var randomIndex = Math.floor(Math.random() * possibleMoves.length);
+  game.move(possibleMoves[randomIndex]);
+  board.position(game.fen());
+
+  window.setTimeout(makeRandomMove, 500);
+};
+
+board = ChessBoard('board', 'start');
+
+window.setTimeout(makeRandomMove, 500);
